588b02c8bf175232f8857f74ff5dd441e5b44e75
Danil Tashkinov <danil.tashkinov@gmail.com>
2015-04-28 10:13:59 +0300

    Bump version to 1.0.0

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_undev_watchers do
   name 'Redmine Undev Watchers Plugin'
   author 'Undev'
   description 'This plugin makes selection of watchers more convenient.'
-  version '0.0.4'
+  version '1.0.0'
   url 'http://github.com/Undev/redmine_undev_watchers' if respond_to?(:url)
   author_url 'http://github.com/Undev'
   requires_redmine :version_or_higher => '0.9.0'
